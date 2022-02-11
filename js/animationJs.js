b4dce1fc2a654fce00e87dfaea2a30a379a26881
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-11 16:44:28 +0600

    много изменении на главной странице. added разделы

--- js/animationJs.js
+++ js/animationJs.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     document.querySelector(".wrapper").style.display = 'none';
-    let timer = 2000;
+    let timer = 1000;
     setTimeout(() => {
         document.getElementById("temno").className = 'animationDontShow';
         document.querySelector(".wrapper").style.display = 'flex';
